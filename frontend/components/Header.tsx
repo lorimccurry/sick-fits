673b276e1ecdce212bf8429ac4eb6eaa53a8c4aa
Lori McCurry <lori.mccurry@gmail.com>
2021-04-07 12:15:24 -0500

    basic global styling and nprogress component

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,17 +1,51 @@
 import Link from 'next/link';
 import { ReactElement } from 'react';
 import Nav from './Nav';
+import styled from 'styled-components';
+
+const LogoStyles = styled.h1`
+  background: red;
+  font-size: 4rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
 
 export default function Header(): ReactElement {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Sick Fits</Link>
+        <LogoStyles>
+          <Link href="/">Sick fits</Link>
+        </LogoStyles>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 import Page from '../components/Page';
+import NProgress from 'nprogress';
+import '../components/styles/nprogress.css';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({
   Component,

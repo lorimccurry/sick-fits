7e0606b5756a6dc43c8bf86f9fc1cfcd93835cbf
Lori McCurry <lori.mccurry@gmail.com>
2021-04-08 16:05:11 -0500

    add product, product image, and seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -4,8 +4,11 @@ import {
   withItemData,
   statelessSessions,
 } from '@keystone-next/keystone/session';
+import { ProductImage } from './schemas/ProductImage';
+import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -35,10 +38,16 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database!!!');
+        if (process.argv.includes('--seed-data'))
+          await insertSeedData(keystone);
+      },
     },
     lists: createSchema({
       User,
+      Product,
+      ProductImage,
     }),
     ui: {
       // TODO: Show the UI only for people that pass this test

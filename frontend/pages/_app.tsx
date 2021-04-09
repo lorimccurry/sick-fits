c673ca279bc982888df50f41e303b175ff7388e8
Lori McCurry <lori.mccurry@gmail.com>
2021-04-09 11:45:25 -0500

    apollo client setup

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,21 +1,40 @@
 import { AppProps } from 'next/app';
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import Page from '../components/Page';
 import NProgress from 'nprogress';
 import '../components/styles/nprogress.css';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
+import {} from '@apollo/client';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({
-  Component,
-  pageProps,
-}: AppProps): ReactElement {
+interface PageProps {
+  query?: string;
+}
+
+// TODO: fix type for props. AppProps & figure out typing for ApolloProvider client
+function MyApp({ Component, pageProps, apollo }: AppProps): ReactElement {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps: PageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+
+  return { pageProps };
+};
+
+export default withData(MyApp);
